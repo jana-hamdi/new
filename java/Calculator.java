19f650cb5ffb877d92905aa90bde324babc4d934
jana-hamdi <jana.is.c0ding@gmail.com>
2025-07-25 13:48:21 +0300

    Create Calculator.java

---
+++ java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = add(a, b);
+        System.out.println("The sum is: " + sum);
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
+ 
